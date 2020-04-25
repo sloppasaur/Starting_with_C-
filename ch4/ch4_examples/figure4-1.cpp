c3cca9aaabd934515b642fafa9f4f8bbad4c81f9
Nathaniel Barrera <nathaniel.barrera39@gmail.com>
2020-04-24 20:18:32 -0500

    Added figure 4-1 from the textbook

---
+++ ch4/ch4_examples/figure4-1.cpp
@@ -0,0 +1,18 @@
+// A program to calculate the area of a rectangle
+
+#include <iostream>
+
+int main() {
+  double length = 0, width = 0, area = 0;
+
+  std::cout << "Enter the length of the rectangle: ";
+  std::cin >> length;
+
+  std::cout << "Enter the width of the rectangle: ";
+  std::cin >> width;
+
+  area = length * width;
+
+  std::cout << "The area is : " << area << std::endl;
+  return 0;
+}
