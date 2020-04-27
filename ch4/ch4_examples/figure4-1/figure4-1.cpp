6a153595432376c83f1b71bab33df4a557cc01d3
Nathaniel Barrera <nathaniel.barrera39@gmail.com>
2020-04-27 15:55:55 -0500

    Added comments

--- ch4/ch4_examples/figure4-1/figure4-1.cpp
+++ ch4/ch4_examples/figure4-1/figure4-1.cpp
@@ -6,12 +6,15 @@
 int main() {
   double length = 0, width = 0, area = 0;
 
+  // length
   std::cout << "Enter the length of the rectangle: ";
   std::cin >> length;
 
+  // width
   std::cout << "Enter the width of the rectangle: ";
   std::cin >> width;
 
+  // area
   area = length * width;
 
   std::cout << "The area is : " << area << std::endl;
