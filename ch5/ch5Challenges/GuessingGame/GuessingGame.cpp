8d88fc87700b3374cfbc26efec2bb2c0bba53eb4
Nathaniel Barrera <nathaniel.barrera39@gmail.com>
2020-05-09 12:59:53 -0500

    Added Challenge 12 for chapter 5

---
+++ ch5/ch5Challenges/GuessingGame/GuessingGame.cpp
@@ -0,0 +1,35 @@
+// Write a program that generates a random number between
+// 1 and 100 and asks the user to guess what the number is.
+// The program should say whether or not the guess is higher
+// or lower than the random number.
+#include <iostream>
+#include <stdlib.h>     // for randoms
+
+int main()
+{
+    int secret = std::rand() % 100 + 1,     // random number
+        guess = 0;                          // user's guess
+
+    std::cout << "I have a number in my head from 1 to 100...\n";
+    std::cout << "Can you guess what it is?" << std::endl;
+
+    do
+    {
+        std::cout << "Take a guess! ";
+        std::cin >> guess;
+
+        if (guess > secret)
+        {
+            std::cout << "That's too high! Try again!\n";
+        }
+        if (guess < secret)
+        {
+            std::cout << "That's too low! Try again!\n";
+        }
+    } while (guess != secret);
+
+    std::cout << "Congratulations! You got the number!\n";
+    std::cout << "Thanks for playing!" << std::endl;
+
+    return 0;
+}
